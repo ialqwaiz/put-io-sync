27dad54290c283c53b7e2a2e394b1709c0cbb398
Ibraheem Alqwaiz <ialqwaiz@users.noreply.github.com>
2019-03-05 20:36:20 +0300

    Update and rename config.sample.js to config.js

--- config.js
+++ config.js
@@ -1,18 +1,20 @@
+//'register an app here https://put.io/v2/oauth2/register and enter the generated oauth2 token here'
 module.exports = {
   'putIo': {
-    'oauth2key': 'register an app here https://put.io/v2/oauth2/register and enter the generated oauth2 token here'
+    //'oauth2key': 'token'
+    'oauth2key': process.env.PUTIO_TOKEN
   },
   'aria2c': {
     'path': 'aria2c',
   },
   'filebot': {
 		'configKey e.g. "tvshows"': {
-			'enable': true,
+			'enable': false,
 			'path': 'filebot',
 			'format': '/some/path/{n}/{n} - {s00e00} - {t}'
 		},
 		'configKey e.g. "movies"': {
-			'enable': true,
+			'enable': false,
 			'path': 'filebot',
 			'format': '/some/path/{n}{t}'
 		}
