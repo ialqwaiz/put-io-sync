b7e4b25822ea02843838e92dba3c392eadb6ca78
Ibraheem Alqwaiz <ialqwaiz@users.noreply.github.com>
2019-03-05 21:07:29 +0300

    Create putio_script.sh

---
+++ putio_script.sh
@@ -0,0 +1,9 @@
+if [ $MOVIES_FOLDER_ID != 0 ]
+then
+  node /put-io-sync/sync.js -d $MOVIES_FOLDER_ID -l /Movies
+fi
+
+if [ $TV_FOLDER_ID != 0 ]
+then
+  node /put-io-sync/sync.js -d $TV_FOLDER_ID -l /TV
+fi
