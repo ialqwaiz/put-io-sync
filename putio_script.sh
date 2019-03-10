9584e59f06a9d27169a17e15faf5254755a54c2c
Ibraheem Alqwaiz <ialqwaiz@users.noreply.github.com>
2019-03-10 16:53:13 +0300

    Update putio_script.sh

--- putio_script.sh
+++ putio_script.sh
@@ -1,9 +1,18 @@
+#!/bin/bash
+date +%r
+
+while :
+do
+
 if [ $MOVIES_FOLDER_ID != 0 ]
 then
-  node /put-io-sync/sync.js -d $MOVIES_FOLDER_ID -l /Movies &
+  node /put-io-sync/sync.js -d $MOVIES_FOLDER_ID -l /Movies >> /putio.log 
 fi
 
 if [ $TV_FOLDER_ID != 0 ]
 then
-  node /put-io-sync/sync.js -d $TV_FOLDER_ID -l /TV &
+  node /put-io-sync/sync.js -d $TV_FOLDER_ID -l /TV >> /putio.log
 fi
+
+sleep 60
+done
